85956b3a345c265cde97a38f6c77a226a5d128f3
Monzer Osman <59767837+monzer15@users.noreply.github.com>
2024-09-07 01:54:59 +0300

    Update shop.blade.php

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,7 +1,7 @@
 <div>
     @if(\Filament\Facades\Filament::getCurrentPanel()->getId() == 'tenant')
         <span class="font-bold text-sm" style="color: #0054ff">
-        <a target="_blank" href="{{ env('SHOP_URL') . \Filament\Facades\Filament::getTenant()->slug }}"> {{ __("fields.shop_link") }}</a>
+        <a target="_blank" href="{{ config('app.shop_url') . \Filament\Facades\Filament::getTenant()->slug }}"> {{ __("fields.shop_link") }}</a>
     </span>
     @endif
 </div>
