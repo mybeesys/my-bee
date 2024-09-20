30de28983609129d98d25d06000694b705448cc5
Monzer Osman <59767837+monzer15@users.noreply.github.com>
2024-09-20 19:17:04 +0300

    Update OrderController.php

--- app/Http/Controllers/API/V1/OrderController.php
+++ app/Http/Controllers/API/V1/OrderController.php
@@ -55,7 +55,8 @@ class OrderController extends BaseController
             ->when($request->from_date or $request->to_date, function (Builder $builder) use ($request) {
                 return $builder->whereDateBetween('created_at', $request->from_date, $request->to_date, "d-m-Y");
             })
-            ->latest();
+            ->orderByDesc('created_at')
+            ->get();
 
         if($request->has('payment_status')){
             $data = $data->filter(function ($order) use ($request){
