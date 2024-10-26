9f771ac49de8630afa55ff0243b349cf98a0731f
Monzer Osman <59767837+monzer15@users.noreply.github.com>
2024-10-26 19:44:59 +0300

    Update StoreController.php

--- app/Http/Controllers/API/V1/StoreController.php
+++ app/Http/Controllers/API/V1/StoreController.php
@@ -783,6 +783,10 @@ class StoreController extends BaseController
 
     protected function generateCartData(array $items): array
     {
+//        $extrasTotal = collect($items)->sum(function ($item) {
+//            return collect($item['extras'])->sum('price');
+//        });
+
         $subTotal = $this->calculateSubTotal($items);
 
         $couponData = $this->getCouponInfo($subTotal);
