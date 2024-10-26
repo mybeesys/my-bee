cc2460c8dc7063b99d73efcfc4500dacf15c19a5
Monzer Osman <59767837+monzer15@users.noreply.github.com>
2024-10-26 20:12:18 +0300

    Update InvoiceItemResource.php

--- app/Http/Resources/InvoiceItemResource.php
+++ app/Http/Resources/InvoiceItemResource.php
@@ -14,6 +14,8 @@ class InvoiceItemResource extends BaseResource
      */
     public function toArray($request): array
     {
+//        'subTotal' => format_amount(($this->price * $this->qty) + $this->tax - $this->discount) . " " . main_currency_native_symbol()
+
         return $this->filterFields([
             'id' => $this->id,
             'name' => $this->product->name,
@@ -21,7 +23,7 @@ class InvoiceItemResource extends BaseResource
             'tax' => format_amount($this->tax) . " " . main_currency_native_symbol(),
             'discount' => format_amount($this->discount) . " " . main_currency_native_symbol(),
             'price' => format_amount($this->price) . " " . main_currency_native_symbol(),
-            'subTotal' => format_amount(($this->price * $this->qty) + $this->tax - $this->discount) . " " . main_currency_native_symbol()
+            'subTotal' => format_amount(($this->price * $this->qty) - $this->discount) . " " . main_currency_native_symbol()
         ]);
     }
 }
