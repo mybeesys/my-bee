6192041d43e723307d1004b950ca816f6474f1d8
Monzer Osman <59767837+monzer15@users.noreply.github.com>
2024-11-17 15:21:11 +0200

    Update StoreController.php

--- app/Http/Controllers/API/V1/StoreController.php
+++ app/Http/Controllers/API/V1/StoreController.php
@@ -793,6 +793,9 @@ class StoreController extends BaseController
 
         $taxes = $this->calculateTaxes(array_values($items), $couponData);
 
+        $subTotal = $subTotal - $taxes;
+        $total = $subTotal + $taxes;
+
         return [
             'subTotal' => $subTotal,
             'subTotalFormatted' => number_format($subTotal, currency_decimals(), '.', ',') . " " . main_currency_native_symbol(),
@@ -800,8 +803,8 @@ class StoreController extends BaseController
             'subTotalFormattedAfterDiscount' => number_format($subTotal - $couponData['amount'], currency_decimals(), '.', ',') . " " . main_currency_native_symbol(),
             'tax' => number_format($taxes, currency_decimals(), '.', ''),
             'taxFormatted' => number_format($taxes, currency_decimals(), '.', ',') . " " . main_currency_native_symbol(),
-            'total' => number_format($subTotal - $couponData['amount'], currency_decimals(), '.', ''),
-            'totalFormated' => number_format($subTotal - $couponData['amount'], currency_decimals(), '.', ',') . " " . main_currency_native_symbol(),
+            'total' => number_format($total - $couponData['amount'], currency_decimals(), '.', ''),
+            'totalFormated' => number_format($total - $couponData['amount'], currency_decimals(), '.', ',') . " " . main_currency_native_symbol(),
             'deliveryFees' => app()->getLocale() == "ar" ? "سيتم إحتساب رسوم التوصيل بناءً على موقعك" : "Delivery fees will be calculated based on your location",
             'coupon' => $couponData,
             "items" => array_values($items)
